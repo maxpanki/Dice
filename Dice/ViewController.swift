d18ae4592c20f9ba235394db244a8022c99548a0
maxpanki <maxrepan@gmail.com>
2019-05-09 01:39:32 +0200

    Add dices, background and Roll button to the main screen

---
+++ Dice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dice
+//
+//  Created by Maksim Pansevich on 5/9/19.
+//  Copyright © 2019 Maksim Pansevich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
