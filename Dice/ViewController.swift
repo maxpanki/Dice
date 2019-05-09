c8283cffc5eedf613d4813dee21c9e45a4f661c2
maxpanki <maxrepan@gmail.com>
2019-05-09 18:58:53 +0200

    Add function that enables roll dices, when the User shake their phone

--- Dice/ViewController.swift
+++ Dice/ViewController.swift
@@ -20,19 +20,31 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        updateDiceIndex()
+        
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         
+        updateDiceIndex()
+
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        
+        updateDiceIndex()
+        
+    }
+    
+    func updateDiceIndex (){
+        
         diceIndex1 = Int.random(in: 0 ... 5)
         diceIndex2 = Int.random(in: 0 ... 5)
         
-        print(diceIndex1)
-        print(diceIndex2)
-        
         diceImageView1.image = UIImage(named: diceArray[diceIndex1])
         diceImageView2.image = UIImage(named: diceArray[diceIndex2])
+        
         
         //OR without an array
         //diceImageView1.image = UIImage(named: "dice\(diceIndex1+1)")
